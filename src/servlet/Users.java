c16d2f5767fd1ce7cc45d7db8b4ab1b709213594
fokia <fokia@qq.com>
2017-09-03 23:11:14 +0800

    完成users模块

--- src/servlet/Users.java
+++ src/servlet/Users.java
@@ -27,7 +27,6 @@ public class Users extends HttpServlet {
 
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        System.out.println("test");
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
         String users = QueryUser.getInstance().queryUsers();
